b159afe513977a9df671c99ce400a43eee8d7686
tuhtinov.a <onlinely@yandex.ru>
2022-12-13 13:50:15 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink} from "react-router-dom";
+import About from './About/About'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink
+                  to="/"
+                  exact
+                  activeClassName={'wfm-active'}
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{
+                color: 'blue'
+              }}>About</NavLink>
+            </li>
+              <li>
+                  <NavLink to="/cars">Cars</NavLink>
+              </li>
+          </ul>
+        </nav>
+
+        <hr/>
+
+          <Route path="/" exact render={() => <h1>Home Page</h1>} />
+          <Route path="/about" component={About} />
+          <Route path="/cars" component={Cars} />
+      </div>
+    );
+  }
+}
+
+export default App
