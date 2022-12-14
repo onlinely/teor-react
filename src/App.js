95b16e8bd4ddd9807b8f243764339a656b74464c
tuhtinov.a <onlinely@yandex.ru>
2022-12-14 15:21:40 +0300

    14.12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, {Component} from 'react'
-import './App.scss'
-import {Route, NavLink} from "react-router-dom";
-import About from './About/About'
-import Cars from './Cars/Cars'
+import React, {Component} from 'react';
+import './App.scss';
+import {Route, NavLink, Switch, Redirect} from "react-router-dom";
+import About from './About/About';
+import Cars from './Cars/Cars';
+import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  }
   render() {
 
     return (
@@ -30,10 +35,20 @@ class App extends Component {
         </nav>
 
         <hr/>
+          <div style={{textAlign: 'center'}}>
+              <h3>Is logged in {this.state.isLoggedIn ? 'TRUE' : 'FALSE'}</h3>
+              <button onClick={() => this.setState({isLoggedIn: true})}>Login</button>
+          </div>
 
-          <Route path="/" exact render={() => <h1>Home Page</h1>} />
-          <Route path="/about" component={About} />
-          <Route path="/cars" component={Cars} />
+        <hr/>
+        <Switch>
+        <Route path="/" exact render={() => <h1>Home Page</h1>}/>
+          { this.state.isLoggedIn ? <Route path="/about" component={About}/> : null}
+          <Route path="/cars/:name" component={CarDetail}/>
+          <Route path="/cars" component={Cars}/>
+          {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>} />*/}
+          <Redirect to={'/'}/>
+        </Switch>
       </div>
     );
   }
--- src/Cars/Cars.js
+++ src/Cars/Cars.js
@@ -10,13 +10,23 @@ export default class Cars extends Component {
     ]
   }
 
+  goToHomePage = () => {
+    this.props.history.push({
+      pathname: '/'
+    })
+  }
+
   render() {
+    // console.log(this.props)
     return (
       <div style={{
         width: 400,
         margin: 'auto',
-        paddingTop: '20px'
+        paddingTop: '20px',
+        textAlign: 'center'
       }}>
+        <button onClick={this.goToHomePage}>Go to home page</button>
+        <hr />
         {this.state.cars.map((car, index) => {
           return (
             <Car
